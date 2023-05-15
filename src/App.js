81d97ecfb8c9de590e308f3c7b9e1e957c3581eb
Fox <taylorcasse@gmail.com>
2023-05-15 18:08:31 +0200

    Making a bit of a mess

--- src/App.js
+++ src/App.js
@@ -1,5 +1,11 @@
+import TodoItem from "./components/TodoItem/TodoItem";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <TodoItem title="A Dummy item" date={new Date("20 January 2020")} />
+    </div>
+  );
 }
 
 export default App;
