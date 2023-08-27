db33aa28c84f171b856d7b90f8a4b30d6aa0d486
Taylor Casse <taylorcasse@gmail.com>
2023-08-27 19:21:17 +0200

    Added media query to hide priority, reformatted Todo component

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -3,18 +3,9 @@ import classes from "./TodoItem.module.css";
 import Button from "../UI/Button/Button";
 
 const parseDate = (dateObj) => {
-  console.log("Date stuff");
-  console.log(dateObj.getDate());
-
-  return dateObj.toString().split("").splice(4, 11).join("");
-};
-
-const parseDate2 = (dateObj) => {
   const date2 = new Date();
   const diffTime = Math.abs(date2 - dateObj);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
-  console.log(diffTime + " milliseconds");
-  console.log(diffDays + " days");
 
   let returnString = "Due in ";
   if (diffDays < 13) {
@@ -39,22 +30,24 @@ const TodoItem = (props) => {
     props.onRemoveItem(props.id);
   };
 
-  parseDate2(props.dueDate);
-
   return (
     <Card>
       <li className={classes.main}>
-        <input type="checkbox" className={classes.checkbox}></input>
-        <h3 className={classes.title}>{props.title}</h3>
+        <div className={classes.leftGroup}>
+          <input type="checkbox" className={classes.checkbox}></input>
 
-        <div className={classes.infoContainer}>
+          <div className={classes.infoContainer}>
+            <h3 className={classes.title}>{props.title}</h3>
+            <div className={classes.dueDate}>{parseDate(props.dueDate)}</div>
+          </div>
+        </div>
+
+        <div className={classes.rightGroup}>
           <div className={classes.priority}>
             {"Priority: " + props.priority}
           </div>
-          <div className={classes.dueDate}>{parseDate2(props.dueDate)}</div>
+          <Button onClick={removeItemHandler}>Remove</Button>
         </div>
-        {/* <button onClick={removeItemHandler}>Remove</button> */}
-        <Button onClick={removeItemHandler}>Remove</Button>
       </li>
     </Card>
   );
