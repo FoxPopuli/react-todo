f41a0c9439ae9a7fff1383dfb2494c1bea49093e
Taylor Casse <taylorcasse@gmail.com>
2023-08-27 22:17:51 +0200

    Implimented clean date fix

--- src/components/TodoItem/TodoItem.js
+++ src/components/TodoItem/TodoItem.js
@@ -6,45 +6,34 @@ import PriorityMarker from "../UI/PriorityMarker/PriorityMarker";
 const parseDate = (dateObj) => {
   const date2 = new Date();
   const isOverdue = date2 - dateObj > 0 ? true : false;
-  console.log(isOverdue);
   const diffTime = Math.abs(date2 - dateObj);
   const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
 
-  if (isOverdue) {
-    let returnString = "Due ";
-    if (diffDays < 13) {
-      returnString += diffDays === 1 ? diffDays + " day" : diffDays + " days";
-    } else if (diffDays < 30) {
-      const numWeeks = Math.floor(diffDays / 7);
-      returnString += numWeeks === 1 ? numWeeks + " week" : numWeeks + " weeks";
-    } else if (diffDays < 365) {
-      const numMonths = Math.floor(diffDays / 30);
-      returnString +=
-        numMonths === 1 ? numMonths + " month" : numMonths + " months";
-    } else {
-      const numYears = Math.floor(diffDays / 365);
-      returnString += numYears === 1 ? numYears + " year" : numYears + " years";
-    }
-
-    return returnString + " ago";
+  let midString;
+  if (diffDays < 13) {
+    midString = diffDays === 1 ? diffDays + " day" : diffDays + " days";
+  } else if (diffDays < 30) {
+    const numWeeks = Math.floor(diffDays / 7);
+    midString = numWeeks === 1 ? numWeeks + " week" : numWeeks + " weeks";
+  } else if (diffDays < 365) {
+    const numMonths = Math.floor(diffDays / 30);
+    midString = numMonths === 1 ? numMonths + " month" : numMonths + " months";
   } else {
-    let returnString = "Due in ";
-    if (diffDays < 13) {
-      returnString += diffDays === 1 ? diffDays + " day" : diffDays + " days";
-    } else if (diffDays < 30) {
-      const numWeeks = Math.floor(diffDays / 7);
-      returnString += numWeeks === 1 ? numWeeks + " week" : numWeeks + " weeks";
-    } else if (diffDays < 365) {
-      const numMonths = Math.floor(diffDays / 30);
-      returnString +=
-        numMonths === 1 ? numMonths + " month" : numMonths + " months";
-    } else {
-      const numYears = Math.floor(diffDays / 365);
-      returnString += numYears === 1 ? numYears + " year" : numYears + " years";
-    }
-
-    return returnString;
+    const numYears = Math.floor(diffDays / 365);
+    midString = numYears === 1 ? numYears + " year" : numYears + " years";
   }
+
+  let firstString;
+  let lastString;
+  if (isOverdue) {
+    firstString = "Due ";
+    lastString = " ago";
+  } else {
+    firstString = "Due in ";
+    lastString = "";
+  }
+
+  return firstString + midString + lastString;
 };
 
 const TodoItem = (props) => {
