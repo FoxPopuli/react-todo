2003455c13bbf3bed11ba586ef528eb1c675112f
Taylor Casse <taylorcasse@gmail.com>
2023-08-30 18:19:13 +0200

    Fixed sorting array mutating original array

--- src/components/UI/TaskGroup/TaskGroup.js
+++ src/components/UI/TaskGroup/TaskGroup.js
@@ -3,31 +3,33 @@ import TodoList from "../../TodoList/TodoList";
 const TaskGroup = (props) => {
   // User specified sorting
 
+  let arrClone = [...props.tasks];
   let sortedTasks;
   switch (props.sortBy) {
     case "alpha":
-      sortedTasks = props.tasks.sort((a, b) => {
+      // spread operater to avoid mutating original array
+      sortedTasks = arrClone.sort((a, b) => {
         const titleA = a.title.toUpperCase();
         const titleB = b.title.toUpperCase();
         return titleA < titleB ? -1 : titleA > titleB ? 1 : 0;
       });
       break;
     case "priority":
-      sortedTasks = props.tasks.sort((a, b) => {
-        const priorityA = +a.prority;
-        const priorityB = +b.prority;
+      sortedTasks = arrClone.sort((a, b) => {
+        const priorityA = a.prority;
+        const priorityB = b.prority;
         return priorityA < priorityB ? -1 : priorityA > priorityB ? 1 : 0;
       });
       break;
     case "date":
-      sortedTasks = props.tasks.sort((a, b) => {
-        const dateA = a.date;
-        const dateB = b.date;
+      sortedTasks = arrClone.sort((a, b) => {
+        const dateA = a.dueDate;
+        const dateB = b.dueDate;
         return dateA < dateB ? -1 : dateA > dateB ? 1 : 0;
       });
       break;
     default:
-      sortedTasks = props.tasks.sort((a, b) => {
+      sortedTasks = arrClone.sort((a, b) => {
         const priorityA = a.prority;
         const priorityB = b.prority;
         return priorityA < priorityB ? -1 : priorityA > priorityB ? 1 : 0;
