2b842e8c74b04cf2dacc8ff6b4dd02782d6b3b04
Taylor Casse <taylorcasse@gmail.com>
2023-10-10 19:22:37 +0200

    Add async onSubmit

--- src/components/SignupForm/SignupForm.js
+++ src/components/SignupForm/SignupForm.js
@@ -1,8 +1,36 @@
 import classes from "./SignupForm.module.css";
 import Card from "../UI/Card/Card";
+import { useNavigate } from "react-router-dom";
 import { useRef } from "react";
+import { NavLink, useNavigate } from 'react-router-dom';
+import {  createUserWithEmailAndPassword  } from 'firebase/auth';
+import { auth } from '../firebase';
 
 const SignupForm = () => {
+
+    const navigate = useNavigate();
+ 
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('');
+ 
+    const onSubmit = async (e) => {
+      e.preventDefault()
+     
+      await createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+            // Signed in
+            const user = userCredential.user;
+            console.log(user);
+            navigate("/login")
+            // ...
+        })
+        .catch((error) => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+            console.log(errorCode, errorMessage);
+            // ..
+        });
+
   return (
     <Card>
       <form>
@@ -11,7 +39,13 @@ const SignupForm = () => {
           <label htmlFor="email">E-mail</label>
           <input type="email" required ref={titleInputRef}></input>
         </div>
+        <div className={classes.formSection}>
+          <label htmlFor="password">E-mail</label>
+          <input type="password" required ref={titleInputRef}></input>
+        </div>
       </form>
     </Card>
   );
 };
+
+export default SignupForm;
