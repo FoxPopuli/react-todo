2d838086851a222be51cdf5bc8700ed0bd61899d
Taylor Casse <taylorcasse@gmail.com>
2023-11-06 17:53:44 +0200

    Fix bad merge

--- src/store/task-context.js
+++ src/store/task-context.js
@@ -1,10 +1,5 @@
 import { createContext, useState, useContext, useEffect } from "react";
 import AuthContext from "./auth-context";
-<<<<<<< Updated upstream
-=======
-import { auth } from "../firebase";
-import tempData from "../data/dummyData";
->>>>>>> Stashed changes
 
 const TaskContext = createContext({
   data: [],
@@ -46,10 +41,6 @@ export const TaskContextProvider = (props) => {
     if (e.key === "Enter") console.log(data);
   });
 
-  useEffect(() => {
-    fetchData();
-  }, []);
-
   // useEffect(() => {
   //   setIsHardLoading(true);
 
@@ -77,7 +68,7 @@ export const TaskContextProvider = (props) => {
   const modifyServerData = (data) => {
     fetch(
       // "https://react-todo-75b5d-default-rtdb.firebaseio.com/test-data.json",
-      `https://react-to-do-development-default-rtdb.firebaseio.com/${auth?.currentUser?.uid}/data.json`,
+      `https://react-to-do-development-default-rtdb.firebaseio.com/${authCtx?.currentUser?.uid}/data.json`,
       {
         method: "PUT",
         body: JSON.stringify(data),
@@ -98,27 +89,9 @@ export const TaskContextProvider = (props) => {
 
   const fetchData = () => {
     setIsHardLoading(true);
-<<<<<<< Updated upstream
-    if (authCtx.currentUser) {
-      fetch(
-        `https://react-to-do-development-default-rtdb.firebaseio.com/${authCtx.currentUser.uid}/data.json`
-      )
-        .then((response) => {
-          return response.json();
-        })
-        .then((serverData) => {
-          setData(serverData);
-          console.log(serverData);
-          setIsHardLoading(false);
-        });
-    } else {
-      console.log("No user logged in.");
-      setIsHardLoading(false);
-    }
-=======
 
     fetch(
-      `https://react-to-do-development-default-rtdb.firebaseio.com/${auth?.currentUser?.uid}/data.json`
+      `https://react-to-do-development-default-rtdb.firebaseio.com/${authCtx?.currentUser?.uid}/data.json`
     )
       .then((response) => {
         return response.json();
@@ -128,7 +101,6 @@ export const TaskContextProvider = (props) => {
         console.log(serverData);
         setIsHardLoading(false);
       });
->>>>>>> Stashed changes
   };
 
   const getIsLoading = () => isLoading;
@@ -200,7 +172,7 @@ export const TaskContextProvider = (props) => {
   const postToServer = (data) => {
     setIsLoading(true);
     fetch(
-      `https://react-to-do-development-default-rtdb.firebaseio.com/${auth?.currentUser?.uid}/data.json`,
+      `https://react-to-do-development-default-rtdb.firebaseio.com/${authCtx?.currentUser?.uid}/data.json`,
 
       {
         method: "POST",
