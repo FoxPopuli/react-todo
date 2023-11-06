9c262d012184581d98bd4bcb69d832cb288053a6
Taylor Casse <taylorcasse@gmail.com>
2023-11-06 18:47:40 +0200

    Add some styling to dropdown

--- src/components/ProfileButton/ProfileButton.js
+++ src/components/ProfileButton/ProfileButton.js
@@ -1,10 +1,17 @@
 import classes from "./ProfileButton.module.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import AuthContext from "../../store/auth-context";
 import blankProfilePic from "../../img/blank-profile-pic.svg";
+import ProfileDropdown from "../ProfileDropdown/ProfileDropdown";
 
 const ProfileButton = () => {
   const authCtx = useContext(AuthContext);
+
+  const [isDropdownHidden, setIsDropdownHidden] = useState(true);
+  const toggleDropdown = () => {
+    setIsDropdownHidden(() => !isDropdownHidden);
+    console.log("isHidden: ", isDropdownHidden);
+  };
 
   const profilePicSrc = authCtx.currentUser.photoURL
     ? authCtx.currentUser.photoURL
@@ -12,7 +19,12 @@ const ProfileButton = () => {
 
   return (
     <div className={classes.main}>
-      <img src={profilePicSrc} className={classes.profileImage}></img>
+      <img
+        src={profilePicSrc}
+        className={classes.profileImage}
+        onClick={toggleDropdown}
+      ></img>
+      <ProfileDropdown isHidden={isDropdownHidden} />
     </div>
   );
 };
--- src/components/ProfileDropdown/ProfileDropdown.js
+++ src/components/ProfileDropdown/ProfileDropdown.js
@@ -1,7 +1,37 @@
 import classes from "./ProfileDropdown.module.css";
+import { useContext, useState, useRef, useEffect } from "react";
+import AuthContext from "../../store/auth-context";
 
-const ProfileDropdown = () => {
-  return <div className={classes.main}></div>;
+const ProfileDropdown = (props) => {
+  const authCtx = useContext(AuthContext);
+  const btnRef = useRef();
+
+  const [isActive, setIsActive] = useState(false);
+
+  const logoutHandler = () => authCtx.logout();
+
+  useEffect(() => {
+    const closeDropdown = (e) => {
+      if (
+        e.composedPath()[0] !== btnRef.current &&
+        e.composedPath()[1] !== btnRef.current
+      ) {
+        setIsActive(false);
+      }
+    };
+    document.body.addEventListener("click", closeDropdown);
+
+    return () => document.body.removeEventListener("click", closeDropdown);
+  }, []);
+
+  return (
+    <div className={`${classes.main} ${!isActive ? classes.hidden : ""}`}>
+      <p>Dropdown!</p>
+      <button onClick={logoutHandler} ref={btnRef}>
+        Log out
+      </button>
+    </div>
+  );
 };
 
 export default ProfileDropdown;
